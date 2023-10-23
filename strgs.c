244003abb70387a5acc51091c3733e064933e5ab
LancelotSam <133297765+LancelotSam@users.noreply.github.com>
2023-10-22 22:26:53 -0400

    Update strgs.c

--- strgs.c
+++ strgs.c
@@ -1,48 +1,56 @@
 #include "monty.h"
 /**
  * print_char-this is the main function
- * it rints the Ascii value of chars
+ * it prints the char at the top of stack
  * @head: the p to the p of the head of stack
  * @line_no: line number in opcode
  * Return:void
  */
 void print_char(stack_t **head, unsigned int line_no)
 {
-	int ascii;
+        stack_t *h;
 
-	if (*head == NULL || (*head)->next == NULL || head == NULL)
-		err_3(11, line_no);
-	ascii = (*head)->n;
-	if (ascii < 0 || ascii > 127)
-		err_3(10, line_no);
-	printf("%c\n", ascii);
+        h = *head;
+        if (!h)
+        {
+                fprintf(stderr, "L%d: can't pchar, stack empty\n", line_no);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        if (h->n > 127 || h->n < 0)
+        {
+                fprintf(stderr, "L%d: can't pchar, value out of range\n", line_no);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        printf("%c\n", h->n);
 }
 
 /**
  * print_str-this is the main function
- * It prints a string
- * @head: p to the p of the head os tack
+ * It prints a string starting from top of the stack
+ * @head:head of stack
+ * @line_no: line number
  * Return: void
  */
-void print_str(stack_t **head)
+void print_str(stack_t **head, unsigned int line_no)
 {
-	int ascii;
-	stack_t *temp;
+        stack_t *h;
+        (void)line_no;
 
-	if (*head == NULL || head == NULL)
-	{
-		printf("\n");
-		return;
-	}
-	temp = *head;
-
-	while (temp)
-	{
-		ascii = temp->n;
-		if (ascii <= 0 || ascii > 127)
-			break;
-		printf("%c", ascii);
-		temp = temp->next;
-	}
-	printf("\n");
+        h = *head;
+        while (h)
+        {
+                if (h->n > 127 || h->n <= 0)
+                {
+                        break;
+                }
+                printf("%c", h->n);
+                h = h->next;
+        }
+        printf("\n");
 }
