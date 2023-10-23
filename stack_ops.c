3fd18b3b0b53c4ba1471b3442d3a3cdc0fd33007
LancelotSam <133297765+LancelotSam@users.noreply.github.com>
2023-10-22 22:25:33 -0400

    Update stack_ops.c

--- stack_ops.c
+++ stack_ops.c
@@ -1,84 +1,90 @@
 #include "monty.h"
 /**
- * pall-this is the main function
+ * f_pall-this is the main function
  * it prints all the elements of a stack
  * @head:the head os the stack
+ * @line_no: unused
  * Return:void
  */
-void pall(const stack_t *head)
+void f_pall(stack_t **head, unsigned int line_no)
 {
-	if (head == NULL)
-		exit(EXIT_FAILURE);
+        stack_t *h;
+        (void) line_no;
 
-	while (head)
-	{
-		printf("%d\n", head->n);
-		head = head->next;
-	}
+        h = *head;
+        if (h == NULL)
+                return;
+        while (h)
+        {
+                printf("%d\n", h->n);
+                h = h->next;
+        }
 }
 
 /**
- *nop-this is the main function
+ * f_nop-this is the main function
  * it does nothing
  * @head:head of the stack
- * @idx: the counter or current index
+ * @line_no: the counter or current index
  * Return:void
  */
-void nop(stack_t **head, unsigned int idx)
+void f_nop(stack_t **head, unsigned int line_no)
 {
-	(void) idx;
-	(void) head;
+        (void) line_no;
+        (void) head;
 }
 
 /**
- * pop-this is the main function
+ * f_pop-this is the main function
  * It deletes the node at the top of the stack
  * @head: the pointer to the pointer of teh head of stack
  * @line_no: the line number in the opcode
  * Return:void
  */
-void pop(stack_t **head, unsigned int line_no)
+void f_pop(stack_t **head, unsigned int line_no)
 {
-	stack_t *temp;
+        stack_t *h;
 
-	if (*head == NULL || head == NULL)
-		err_2(7, line_no);
-
-	tmp = *head;
-	*head = tmp->next;
-	if (*head != NULL)
-		(*head)->prev = NULL;
-	free(tmp);
+        if (*head == NULL)
+        {
+                fprintf(stderr, "L%d: can't pop an empty stack\n", line_no);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        h = *head;
+        *head = h->next;
+        free(h);
 }
-
 /**
- * pint-this is the main function
+ * f_top-this is the main function
  * it prints the head node at the top of the list
  * @head:pointer to the pointer to the head of the list
  * @line_no: the line_no on opcode
  * Return:void
  */
-void pint(stack_t **head, unsigned int line_no)
+void f_top(stack_t **head, unsigned int line_no)
 {
-	if (*head == NULL || head == NULL)
-		err_2(6, line_no);
-	printf("%d\n", (*head)->n);
+        (void)head;
+        (void)line_no;
+        bus.lifi = 0;
 }
 
 /**
-   * free_stack-this is the main function
-    * it frees the stack
-     * @head: this is the head of the stack
-      * Return:void
-       */
+ * free_stack-this is the main function
+ * it frees the stack
+ * @head: this is the head of the stack
+ * Return:void
+ */
 void free_stack(stack_t *head)
 {
-	stack_t *temp;
+        stack_t *temp;
 
-	while (head)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
+        while (head)
+        {
+                temp = head->next;
+                free(head);
+                head = temp;
+        }
 }
