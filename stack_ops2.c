e4a7276a66e8bf88753add299a2a2970b59c5b07
LancelotSam <133297765+LancelotSam@users.noreply.github.com>
2023-10-22 22:26:26 -0400

    Update stack_ops2.c

--- stack_ops2.c
+++ stack_ops2.c
@@ -1,28 +1,41 @@
-#include "monty.h"
 /**
- * push-this is the main function
+ * f_push-this is the main function
  * it pushes elements to the top of the stack
- * @new_node: pointer to the po of a newly created node
+ * @head: stack head
+ * @line_no: the line number
  * Return:void
  */
-void push(stack_t **new_node)
+void f_push(stack_t **head, unsigned int line_no)
 {
-	stack_t *temp;
+        int n, j = 0, flag = 0;
 
-	if (*new_node == NULL || new_node == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
-	if (head == NULL) /* if list is empty*/
-	{
-		head = *new_node;
-		return;
-	}
-	temp = head;
-	head = *new_node;
-	head->next = temp;
-	temp->prev = head;
+        if (bus.arg)
+        {
+                if (bus.arg[0] == '-')
+                        j++;
+                for (; bus.arg[j] != '\0'; j++)
+                {
+                        if (bus.arg[j] > 57 || bus.arg[j] < 48)
+                                flag = 1; }
+                if (flag == 1)
+                { fprintf(stderr, "L%d: usage: push integer\n", line_no);
+                        fclose(bus.file);
+                        free(bus.content);
+                        free_stack(*head);
+                        exit(EXIT_FAILURE); }}
+        else
+        { fprintf(stderr, "L%d: usage: push integer\n", line_no);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE); }
+        n = atoi(bus.arg);
+        if (bus.lifi == 0)
+                newhead(head, n);
+        else
+                addqueue(head, n);
 }
+
 /**
  * newhead-this is the main function
  * it adds a node to the head of the stack
@@ -32,38 +45,20 @@ void push(stack_t **new_node)
  */
 void newhead(stack_t **head, int n)
 {
-	stack_t *new_head;
+        stack_t *new_head;
 
-	new_head = malloc(sizeof(stack_t));
-	if (new_head == NULL)
-	{
-		frintf(stderr, "Error\n");
-		exit(0);
-	}
-	new_head->n = n;
-	new_head->prev = NULL;
-	new_head->next = *head;
-	if (*head != NULL) /* not an empty list*/
-		(*head)->prev = new_head;
-	*head = new_head; /*mak new_head the current head*/
-}
-/**
- * create_node-this is the main function
- * it creates a new node
- * @n: the data to be contained in the new node
- * Return:pointer to the node, or NULL
- */
-stack_t *create_node(int n)
-{
-	stack_t *new_node;
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-		err(4);
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-	return (new_node);
+        new_head = malloc(sizeof(stack_t));
+        if (new_head == NULL)
+        {
+                fprintf(stderr, "Error\n");
+                exit(0);
+        }
+        new_head->n = n;
+        new_head->prev = NULL;
+        new_head->next = *head;
+        if (*head != NULL) /* not an empty list*/
+                (*head)->prev = new_head;
+ *head = new_head; /*mak new_head the current head*/
 }
 
 /**
@@ -75,15 +70,34 @@ stack_t *create_node(int n)
  */
 void mod_nodes(stack_t **head, unsigned int line_no)
 {
-	int mod;
+        stack_t *h;
+        int len = 0, aux;
 
-	if (*head == NULL || (*head)->next == NULL || head == NULL)
-		err_2(8, line_no, "mod");
-	if ((*head)->n == 0)
-		err_2(, line_no);
-	(head) = (*head)->next;
-	mod = (*head)->n % (*head)->prev->n;
-	(*head)->n = mod;
-	free((*head)->prev);
-	(*head)->prev = NULL;
+        h = *head;
+        while (h)
+        {
+                h = h->next;
+                len++;
+        }
+        if (len < 2)
+        {
+                fprintf(stderr, "L%d: can't mod, stack too short\n", line_no);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        h = *head;
+        if (h->n == 0)
+        {
+                fprintf(stderr, "L%d: division by zero\n", line_no);
+                fclose(bus.file);
+                free(bus.content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        aux = h->next->n % h->n;
+        h->next->n = aux;
+        *head = h->next;
+        free(h);
 }
